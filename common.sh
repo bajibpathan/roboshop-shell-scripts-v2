ee279a213e6534ce3bf09f5390b88516c8d47a00
Baji Pathan <bajibpthan@gmail.com>
2025-05-22 23:52:38 +0000

    Updated catalogue script

--- common.sh
+++ common.sh
@@ -40,6 +40,55 @@ VALIDATE(){
         exit 1
     fi
 }
+NODEJS_SETUP(){
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Disabling NodeJS default module"
+
+    dnf module enable nodejs:20 -y &>>$LOG_FILE
+    VALIDATE $? "Enabling NodeJS default module"
+
+    dnf install nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Installing NodeJS"
+
+    npm install &>>$LOG_FILE
+    VALIDATE $? "Installing dependencies"
+
+}
+
+SYSTEM_USER_SETUP(){
+    id roboshop &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+        VALIDATE $? "Creating a roboshop system user"
+    else
+        echo -e "System user roboshop already created ... $YELLOW SKIPPING $NOCOLOR" 
+    fi
+}
+
+APP_SETUP(){
+    mkdir -p /app
+    VALIDATE $? "Creating /app directory"
+
+    curl -o /tmp/$APP_NAME.zip https://roboshop-artifacts.s3.amazonaws.com/$APP_NAME-v3.zip &>>$LOG_FILE
+    VALIDATE $? "Downloading $APP_NAME code to temp directory"
+
+    rm -rf /app/*
+    cd /app 
+    unzip /tmp/$APP_NAME.zip &>>$LOG_FILE
+    VALIDATE $? "Extracting the $APP_NAME code to /app directory"
+}
+
+SYSTEMD_SETUP(){
+    cp $SCRIPT_DIR/services/$APP_NAME.service /etc/systemd/system/$APP_NAME.service
+    VALIDATE $? "Copying $APP_NAME serivce to systemd directory"
+
+    systemctl daemon-reload &>>$LOG_FILE
+    systemctl enable $APP_NAME &>>$LOG_FILE
+    systemctl start $APP_NAME &>>$LOG_FILE
+    VALIDATE $? "Enabling & Starting $APP_NAME service"
+
+}
 
 # To print the script execution time and status
 PRINT_TIME(){
